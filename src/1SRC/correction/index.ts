500ba6a724c5447a3794ff5630e5ab5163b2d043
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2020-09-11 13:54:00 +0100

    srp update

---
+++ src/1SRC/correction/index.ts
@@ -0,0 +1,19 @@
+class Order {
+    public  calculateTotalSum(){ /*...*/}
+    public  getItems(){ /*...*/}
+    public  getItemsCount(){ /*...*/}
+    public  addItem(item: any){ /*...*/}
+    public  deleteItem(item: any){ /*...*/}
+}
+
+class OrderRepository {
+    public load() { /** */}
+    public save() { /** */}
+    public update() { /** */}
+    public delete() { /** */}
+}
+
+class OrderViewer {
+    public printOrder(){ /** */}
+    public showOrder(){ /** */}
+}
---
+++ src/1SRC/violation/index.ts
@@ -0,0 +1,15 @@
+class Order {
+    public  calculateTotalSum(){ /*...*/}
+    public  getItems(){ /*...*/}
+    public  getItemsCount(){ /*...*/}
+    public  addItem(item: any){ /*...*/}
+    public  deleteItem(item: any){ /*...*/}
+
+    public printOrder(){ /** */}
+    public showOrder(){ /** */}
+
+    public load() { /** */}
+    public save() { /** */}
+    public update() { /** */}
+    public delete() { /** */}
+}
